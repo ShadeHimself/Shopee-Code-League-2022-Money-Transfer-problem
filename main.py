f2534213eaa66dadb9b33aa090a9ecd94bc75ffd
Z3R0 <timothyzhang51@gmail.com>
2022-03-19 16:02:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 data = [(3, 4)
-(amir, 10)
-(brenda, 10)
-(charlie, 10)
-(amir, brenda, 5)
-(brenda, charlie, 5)
-(charlie, amir, 20)
-(charlie, amir, 7)
+("amir", 10)
+("brenda", 10)
+("charlie", 10)
+("amir", "brenda", 5)
+("brenda", "charlie", 5)
+("charlie", "amir", 20)
+("charlie", "amir", 7)
 ]
 
 class User:
-    
+    def __init__(self, name, amount):
+        self.name = name
+        self.amount = amount 
+        
